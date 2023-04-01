25d874c673a431d07e362e5a7a5714a4acb67d07
Mamta <120008345+Mamta13kunal@users.noreply.github.com>
2023-04-02 00:30:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-//your JS code here. If required.
-const app = require("./index.js");
-app.listen(3000, () => {
-  console.log('server started');
-});
+const form = document.querySelector('form');
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const submitBtn = document.getElementById('btn');
+
+function validateInputs() {
+  if (nameInput.value === '' || ageInput.value === '') {
+    alert('Please fill in all fields.');
+    return false;
+  }
+  return true;
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  if (!validateInputs()) {
+    return;
+  }
+  const age = parseInt(ageInput.value);
+  const name = nameInput.value;
+  const promise = new Promise((resolve, reject) => {
+    if (age >= 18) {
+      setTimeout(() => {
+        resolve(`Welcome, ${name}. You can vote.`);
+      }, 4000);
+    } else {
+      reject(`Oh sorry ${name}. You aren't old enough.`);
+    }
+  });
+  promise
+    .then((message) => alert(message))
+    .catch((error) => alert(error));
+}
+
+submitBtn.addEventListener('click', handleFormSubmit);
